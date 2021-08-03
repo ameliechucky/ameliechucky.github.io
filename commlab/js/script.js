90bca5757954f8c3aef41e94de99491337d223f0
ameliechucky <mar_c_le@yahoo.com>
2021-08-03 12:07:09 +0800

    commlab page
    página con los trabajos de los estudiantes

---
+++ commlab/js/script.js
@@ -0,0 +1,257 @@
+var nombres = [
+ "Jennifer Araya",
+ "Leonardo Ávila",
+ "Marcelo Becerra",
+ "José Pablo Calderón",
+ "Francisca Castro",
+ "Javiera Contreras",
+ "Benjamín Godoy",
+ "Jorge González",
+ "Tamara Gutiérrez",
+ "Patricia Letelier",
+ "Ma Luisa Lorca",
+ "Antonia Moraga",
+ "Diego Morales",
+ "Laura Muñoz",
+ "Javiera Navarrete",
+ "Gabriela Neira",
+ "Javiera Osorio",
+ "Carlos Piña",
+ "Catalina Reyes",
+ "Antonia Rojas",
+ "Trinidad Romero",
+ "Isidora Sanchez",
+ "Elías Seitz",
+ "Karina Tarifeño",
+ "Anaís Valdés",
+ "Ma Magdalena Valencia"
+];
+
+var github = [
+"https://github.com/guineveree",
+"https://github.com/derthel",
+"https://github.com/marcelobecerra99",
+"#",
+"https://github.com/francarr",
+"https://github.com/javieraccch",
+"#",
+"https://github.com/jorrrge04",
+"https://github.com/tamguties",
+"https://github.com/pati798",
+"https://github.com/mllc99",
+"https://github.com/antomoragas",
+"https://github.com/diegomoralesgajardo",
+"https://github.com/laura2384",
+"https://github.com/j4v1n",
+"https://github.com/gabrielaneiraj",
+"https://github.com/parawiita",
+"https://github.com/carlospinay",
+"https://github.com/catalina-reyes",
+"#",
+"https://github.com/mromerotrini21",
+"https://github.com/isisanchez",
+"https://github.com/el4i4s",
+"https://github.com/karinatnl",
+"https://github.com/anais759",
+"#"
+];
+
+var encargo1 = [
+"https://guineveree.github.io/Guinevereee-github.io/commlab/semana1",
+"https://derthel.github.io/commlab/semana_01",
+"https://marcelobecerra99.github.io/commlab/encargo%201",
+"#",
+"https://francarr.github.io/francarr-githun.io/commlab/semana1",
+"https://javieraccch.github.io/commlab/Semana1",
+"#",
+"https://jorrrge04.github.io/commlab/encargo1",
+"https://tamguties.github.io/commlab/semana1",
+"https://pati798.github.io/pati798-github.io/commlab/SEMANA1",
+"https://mllc99.github.io/commlab/semana1",
+"https://github.com/antomoragas",
+"https://diegomoralesgajardo.github.io/commlab/s1",
+"https://laura2384.github.io/commmlab/semana1.1",
+"https://j4v1n.github.io/commlab/semana1",
+"https://gabrielaneiraj.github.io/CommLab/Encargo1",
+"https://parawiita.github.io/commlab/S1",
+"https://carlospinay.github.io/commlab/semana1",
+"https://catalina-reyes.github.io/Commlab/index.html",
+"#",
+"https://github.com/mromerotrini21",
+"https://isisanchez.github.io/commlab/semana1",
+"https://el4i4s.github.io/commlab/SEMANA_01",
+"https://karinatnl.github.io/Commlab/Semana1",
+"https://anais759.github.io/commlab/semana1",
+"#"
+];
+
+var encargo2 = [
+  "https://guineveree.github.io/Guinevereee-github.io/commlab/semana2",
+"#",
+"https://jorrrge04.github.io/commlab/encargo2",
+"#",
+"https://francarr.github.io/francarr-githun.io/commlab/semana2",
+"https://javieraccch.github.io/commlab/semana2",
+"https://el4i4s.github.io/semana2",
+"https://jorrrge04.github.io/commlab/encargo2",
+"#",
+"https://francarr.github.io/francarr-githun.io/commlab/semana2",
+"https://mllc99.github.io/commlab/semana2",
+"https://mllc99.github.io/commlab/semana2",
+"https://magdavq.github.io/semana2correccion",
+"https://gabrielaneiraj.github.io/CommLab/Semana%202",
+"https://j4v1n.github.io/commlab/semana2",
+"https://gabrielaneiraj.github.io/CommLab/Semana%202",
+"https://j4v1n.github.io/commlab/semana2",
+"https://karinatnl.github.io/comictoby.github.io/Commlab/Semana2",
+"https://catalina-reyes.github.io/commlab/semana2",
+"#",
+"https://javieraccch.github.io/commlab/semana2",
+"https://guineveree.github.io/Guinevereee-github.io/commlab/semana2",
+"https://el4i4s.github.io/semana2",
+"https://karinatnl.github.io/comictoby.github.io/Commlab/Semana2",
+"https://catalina-reyes.github.io/commlab/semana2",
+"https://magdavq.github.io/semana2correccion"
+];
+
+var encargo3 = [
+  "https://isisanchez.github.io/commlab/semana3/",
+  "#",
+  "#",
+  "#",
+  "https://francarr.github.io/francarr-githun.io/commlab/semana3/",
+  "https://mromerotrini21.github.io/commlab/semana3.1/",
+  "#",
+  "#",
+  "https://gabrielaneiraj.github.io/CommLab/Semana3/",
+  "https://francarr.github.io/francarr-githun.io/commlab/semana3/",
+  "https://mllc99.github.io/commlab/semana3/",
+  "https://mllc99.github.io/commlab/semana3/",
+  "https://diegomoralesgajardo.github.io/commlab/s3/",
+  "https://gabrielaneiraj.github.io/CommLab/Semana3/",
+  "https://j4v1n.github.io/commlab/semana%203/",
+  "https://gabrielaneiraj.github.io/CommLab/Semana3/",
+  "https://j4v1n.github.io/commlab/semana%203/",
+  "#",
+  "https://catalina-reyes.github.io/commlab/semana3/",
+  "https://diegomoralesgajardo.github.io/commlab/s3/",
+  "https://mromerotrini21.github.io/commlab/semana3.1/",
+  "https://isisanchez.github.io/commlab/semana3/",
+  "#",
+  "https://karinatnl.github.io/Audiomedios.github.io/Commlab/Semana3/",
+  "https://catalina-reyes.github.io/commlab/semana3/",
+  "#"
+];
+
+for (i = 0; i < nombres.length; i++) {
+
+  var d = document.createElement("DIV");
+  d.className = "box";
+  var li = document.createElement("LI");
+  d.appendChild(li);
+  li.className = "caret";
+  var n = document.createTextNode(nombres[i]);
+  document.body.appendChild(d);
+  li.appendChild(n);
+
+  var ul = document.createElement("UL");
+  ul.className = "nested";
+  li.appendChild(ul);
+  // link 1
+  var l1 = document.createElement("LI");
+  ul.appendChild(l1);
+  var gitLink = document.createElement("A");
+  gitLink.href = github[i];
+  gitLink.target = '_blank';
+  l1.appendChild(gitLink);
+  var git = document.createTextNode("github");
+  gitLink.appendChild(git);
+  // link 2
+  var l2 = document.createElement("LI");
+  ul.appendChild(l2);
+  var e1 = document.createElement("A");
+  e1.href = encargo1[i];
+  e1.target = '_blank';
+  l2.appendChild(e1);
+  var enc1 = document.createTextNode("encargo 1");
+  e1.appendChild(enc1);
+  // link 3
+  var l3 = document.createElement("LI");
+  ul.appendChild(l3);
+  var e2 = document.createElement("A");
+  e2.href = encargo2[i];
+  e2.target = '_blank';
+  l3.appendChild(e2);
+  var enc2 = document.createTextNode("encargo 2");
+  e2.appendChild(enc2);
+  // link 4
+  var l4 = document.createElement("LI");
+  ul.appendChild(l4);
+  var e3 = document.createElement("A");
+  e3.href = encargo3[i];
+  e3.target = '_blank';
+  l4.appendChild(e3);
+  var enc3 = document.createTextNode("encargo 3");
+  e3.appendChild(enc3);
+  // link 5
+  var l5 = document.createElement("LI");
+  ul.appendChild(l5);
+  // var e4 = document.createElement("A");
+  // e4.href = encargo4[i];
+  // e4.target = '_blank';
+  // l5.appendChild(e4);
+  var enc4 = document.createTextNode("final");
+  //e4.appendChild(enc4);
+  l5.appendChild(enc4);
+
+  var br = document.createElement("BR");
+  document.body.appendChild(br);
+
+  // d.style.backgroundColor = "rgb(255,0,0)";
+  d.style.position = "absolute";
+  //d.style.fontFamily = "Helvetica";
+  var s = 12 + Math.floor(Math.random() * 30);
+  var posx = Math.floor(Math.random() * 80 + 10);
+  var posy = Math.floor(Math.random() * 80 + 10);
+  d.style.fontSize = s + "px";
+  ul.style.fontSize = Math.floor(s*.5) + "px";
+  ul.style.marginTop = Math.floor(s*.5) + "px";
+  ul.style.paddingLeft = Math.floor(s*1.5)+"px";
+  d.style.left = posx + "%";
+  d.style.top = posy + "%";
+
+}
+
+var toggler = document.getElementsByClassName("caret");
+var u;
+for (u = 0; u < toggler.length; u++) {
+  toggler[u].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.parentElement.querySelector(".caret").classList.toggle("active");
+  });
+}
+
+interact('.caret').draggable({
+    // enable autoScroll
+  autoScroll: true,
+  listeners: {
+   move: dragMoveListener,
+  }
+});
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
